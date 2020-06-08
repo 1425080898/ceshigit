bfd1a4cb666fd57f847b42a3ce8df3bf40f66bbc
你脸真肥 <1425080898@qq.com>
2020-06-08 19:30:58 +0800

    提交人：小刘 提交原因：修改aaa

--- src/main/java/com/example/ceshigit/CeshigitApplication.java
+++ src/main/java/com/example/ceshigit/CeshigitApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //测试git
-//第一次使用git提交修改代码
+//第一次使用git提交修改代码aaa
 @SpringBootApplication
 public class CeshigitApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CeshigitApplication.class, args);
     }
