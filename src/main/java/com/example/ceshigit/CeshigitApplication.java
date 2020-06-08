1970d4f92c3df6e779568aa2b6353b29a47a43c2
你脸真肥 <1425080898@qq.com>
2020-06-08 19:03:47 +0800

    提交人：小刘 提交原因：测试

---
+++ src/main/java/com/example/ceshigit/CeshigitApplication.java
@@ -0,0 +1,14 @@
+package com.example.ceshigit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//测试git
+@SpringBootApplication
+public class CeshigitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeshigitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ceshigit/CeshigitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ceshigit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeshigitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
